beaa6c3448d70a0951c57169822a8c43965253c5
bumuthuabeygunawardhana <167986397+bumuthuabeygunawardhana@users.noreply.github.com>
2025-08-17 17:58:34 +0530

    created new admin features like edit & view vehicls according to the catagory and also delete vehicle details and view vehicle details

--- src/routes/vehicleRoutes.js
+++ src/routes/vehicleRoutes.js
@@ -1,6 +1,6 @@
 // src/routes/vehicleRoutes.js
 import express from "express";
-import { registerVehicle } from "../controllers/vehicleController.js";
+import { registerVehicle,getVehicleCategories,getVehiclesByCategory,getVehicleById,updateVehicle,deleteVehicle } from "../controllers/vehicleController.js";
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 
@@ -14,5 +14,26 @@ router.post(
   upload.array("photos", 10), // up to 10 images
   registerVehicle
 );
+
+// Get all distinct categories
+router.get("/admin/categories", protect, adminOnly, getVehicleCategories);
+
+// Get vehicles by category (only name + photo)
+router.get("/admin/category/:category", protect, adminOnly, getVehiclesByCategory);
+
+// Get full details of a vehicle by ID
+router.get("/admin/:id", protect, adminOnly, getVehicleById);
+
+// Update vehicle details
+router.put(
+  "/admin/:id",
+  protect,
+  adminOnly,
+  upload.array("photos", 10), // optional photos
+  updateVehicle
+);
+
+// Delete vehicle
+router.delete("/admin/:id", protect, adminOnly, deleteVehicle);
 
 export default router;
